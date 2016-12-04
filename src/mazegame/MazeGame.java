cb90868e7466aeaef632f6a70b6ba3e861dbfaa5
joshuapickenpaugh <joshuapickenpaugh@hotmail.com>
2016-12-04 12:47:50 -0600

    First commit of Maze Game.

---
+++ src/mazegame/MazeGame.java
@@ -0,0 +1,24 @@
+/*
+Joshua Pickenpaugh
+Maze Game
+December 4th, 2016
+ */
+package mazegame;
+import java.util.Scanner;
+
+public class MazeGame {
+
+    public static void main(String[] args) {
+        
+        //Variables:
+        
+        //Size of the maze:
+        final int ROWS = 10;
+        final int COLS = 10;
+        int[][] maze = new int [ROWS][COLS];                
+                
+        //create a Scanner object to read from the keyboard
+	Scanner keyboard = new Scanner(System.in);        
+    }
+    
+}
