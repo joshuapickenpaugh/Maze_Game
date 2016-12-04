687f1835d7ab4c5a2c1e96639915e1aa67a89f82
joshuapickenpaugh <joshuapickenpaugh@hotmail.com>
2016-12-04 17:15:11 -0600

    Commit of Maze Game.

--- src/mazegame/MazeGame.java
+++ src/mazegame/MazeGame.java
@@ -11,6 +11,8 @@ public class MazeGame {
     public static void main(String[] args) {
         
         //Variables:
+        int intUserChoice;
+        int quit = 1;
         
         //Size of the maze:
         final int ROWS = 10;
@@ -19,6 +21,33 @@ public class MazeGame {
                 
         //create a Scanner object to read from the keyboard
 	Scanner keyboard = new Scanner(System.in);        
+        
+        do
+        {
+            System.out.println("Choose an option:");
+            System.out.println("_________________");
+            System.out.println("1: View Inventory");
+            System.out.println("9: QUIT");
+
+            System.out.println();
+            intUserChoice = keyboard.nextInt();     
+            System.out.println();
+            
+            switch(intUserChoice)
+            {
+                case 1:
+                    Inventory.ViewInventory();
+                    break;
+                case 9:
+                    quit = 0;
+                    break;
+                default:
+                    System.out.println("Invalid key. Please make a selection...");
+            }
+            
+        }while(quit == 1);
+        
+        System.out.println("Goodbye, player.");
     }
     
 }
