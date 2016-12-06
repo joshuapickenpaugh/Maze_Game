c91e520366998cfff36b7b538a69b918fa99fb88
joshuapickenpaugh <joshuapickenpaugh@hotmail.com>
2016-12-06 15:17:58 -0600

    Commit of Maze Game.

--- src/mazegame/Inventory.java
+++ src/mazegame/Inventory.java
@@ -16,12 +16,26 @@ public class Inventory
     
     public void AddItemToInventory()
     {
-        
+        if (inventory.length > 4)
+        {
+            System.out.println("You already have the maximum 3 items.");
+        }
+        else
+        {
+            //if picked up an item, then add it to the inventory:
+        }
     }
     
     public void DropItemToInventory()
     {
-        
+        if (inventory.length < 0)
+        {
+            System.out.println("You don't have any items in your invemtory to drop.");
+        }
+        else
+        {
+            //Go to a specific item in the inventory, and drop it:
+        }
     }
    
 }
